fcd64f69576ccc28009cdca94087a6ebc845f0ef
Kirill Gerasimenko <kirill.gerasimenko@dataart.com>
2017-08-24 22:52:27 +0200

    Add project files.

---
+++ TestMutateValidation/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace TestMutateValidation.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("TestMutateValidation")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Dell Inc.")>]
+[<assembly: AssemblyProduct("TestMutateValidation")>]
+[<assembly: AssemblyCopyright("Copyright © Dell Inc. 2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("e13418b7-4690-4f37-a06d-c5de9037d1ce")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ TestMutateValidation/Program.fs
@@ -0,0 +1,40 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System.Windows
+open System
+
+open FsXaml 
+open Gjallarhorn
+open Gjallarhorn.Bindable
+open Gjallarhorn.Validation
+open Gjallarhorn.Wpf
+
+type Window = XAML<"SampleView.xaml">
+
+let createViewModel () =
+    let bindingSource = Binding.createObservableSource ()
+
+    let validator = Validation.Validators.hasLengthAtLeast 3
+
+    let mutable1 = Mutable.create ""
+    let mutable2 = Mutable.create ""
+
+    mutable1 |> Binding.mutateToFromViewValidated bindingSource "Text1" validator
+    mutable2 |> Binding.toFromViewValidated bindingSource "Text2" validator |> ignore
+
+    bindingSource
+
+[<STAThread>]
+[<EntryPoint>]
+let main _ = 
+    Platform.install true |> ignore
+
+    let app = Application ()
+    let win = Window ()
+
+    win.DataContext <- createViewModel ()
+
+    app.Run win |> ignore
+
+    0 
